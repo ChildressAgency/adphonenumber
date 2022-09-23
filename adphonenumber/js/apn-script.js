08825abb808a8b46e28bfbe5200351332612c983
childressdev <jcampbell@childressagency.com>
2022-09-23 14:33:05 -0400

    removed prod folder

--- adphonenumber/js/apn-script.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-  if(adPhone.phone_number !== ''){
-    $('a[href^="tel:"]').each(function(){
-      if(!$(this).hasClass('emergency-phone')){
-        $(this).text(adPhone.phone_number).attr('href', 'tel:' + adPhone.phone_number);
-      }
-    });
-  }
-});
